e1c0151d996a431ff68c5b76badb4d0f59d272ee
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-07-13 12:56:02 -0400

    Added Twitter API keys

--- keys.js
+++ keys.js
@@ -1,9 +1,10 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'phQXEIvvaTyJBjAYUGxaEKqTt',
+  consumer_secret: 'fZTOFKZMIzVkZMZRXcvimsjnznPryK5N2DZoyXR9itNvLidhYw',
+  access_token_key: '727654348138946560-sGKcBPWyIVxiFljSne3ylLHEwqETlq5',
+  access_token_secret: 'lvTZUVlyv6izLXZ3Qtifax7XJFwniV0jzrRvtfEmfqVtC',
 }
 
+//Twitter API: https://apps.twitter.com/app/12601851
